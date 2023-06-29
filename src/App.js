8bd4764b1973ffbe49e44f3ccb736b5cf577c217
Gauravbharti1998 <gaurav.bharti1998@gmail.com>
2023-06-29 15:14:29 +0530

    ADDED STYLE

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react'
 
 function App() {
-  const[val, setVal] = useState("0");
+  const [val, setVal] = useState("0");
 
-  const onChangeHandler=(e)=>{
-    setVal( e.target.value); 
-  };    
-  const acbtn=()=>{
+  const onChangeHandler = (e) => {
+    setVal(e.target.value);
+  };
+  const acbtn = () => {
     setVal("0");
   }
-  const equalbtn=()=>{
+  const equalbtn = () => {
     //eslint-disable-next-line
     setVal(eval(val));
   }
-  const addInput=(inp)=>
-  {
+  const addInput = (inp) => {
     if (val === '0') {
-      setVal( inp);
+      setVal(inp);
     } else {
       setVal(val + inp);
     }
   }
-  const backspace=()=>{
-    setVal(val.substr(0,val.length-1));
+  const backspace = () => {
+    setVal(val.substring(0, val.length - 1));
   }
   const backsp = "Del"
-  return (  
-    <div>
-      <h1>calculator</h1>
-      <input  onChange={e => onChangeHandler(e)} value={val}/>
-      <br/>
-      <button onClick={acbtn}>AC</button>
-      <button onClick={e=>addInput("+")}>+</button>
-      <button onClick={e=>addInput("-")}>-</button>
-      <button onClick={equalbtn}>=</button>
-      <br/>
-      <button onClick={e=>addInput("1")}>1</button>
-      <button onClick={e=>addInput("2")}>2</button>
-      <button onClick={e=>addInput("3")}>3</button>
-      <button onClick={e=>addInput("0")}>0</button>
-      <br/>
-      <button onClick={e=>addInput("4")}>4</button>
-      <button onClick={e=>addInput("5")}>5</button>
-      <button onClick={e=>addInput("6")}>6</button>
-      <button onClick={e=>addInput("/")}>/</button>
-      <br/>
-      <button onClick={e=>addInput("7")}>7</button>
-      <button onClick={e=>addInput("8")}>8</button>
-      <button onClick={e=>addInput("9")}>9</button>
-      <button onClick={e=>addInput("*")}>x</button>
-      <button onClick={e=>backspace()}>{backsp}</button>
+  return (
+    <div className='overall'>
+      <h1>C A L C U L A T O R</h1>
+      <div className='inputsc'>
+        <input className='screen' onChange={e => onChangeHandler(e)} value={val} />
+      </div>
+      <div className='nonscreen'>
+        <button className='buttons' onClick={acbtn}>AC</button>
+        <button className='buttons' onClick={e => addInput("(")}>(</button>
+        <button className='buttons' onClick={e => addInput(")")}>)</button>
+        <button className='buttons' onClick={e => backspace()}>{backsp}</button>
+        <br />
+        <button className='buttons' onClick={e => addInput("1")}>1</button>
+        <button className='buttons' onClick={e => addInput("2")}>2</button>
+        <button className='buttons' onClick={e => addInput("3")}>3</button>
+        <button className='buttons' onClick={equalbtn}>=</button>
+        <br />
+        <button className='buttons' onClick={e => addInput("4")}>4</button>
+        <button className='buttons' onClick={e => addInput("5")}>5</button>
+        <button className='buttons' onClick={e => addInput("6")}>6</button>
+        <button className='buttons' onClick={e => addInput("/")}>/</button>
+        <br />
+        <button className='buttons' onClick={e => addInput("7")}>7</button>
+        <button className='buttons' onClick={e => addInput("8")}>8</button>
+        <button className='buttons' onClick={e => addInput("9")}>9</button>
+        <button className='buttons' onClick={e => addInput("*")}>x</button>
+        <br/>
+        <button className='buttons' onClick={e => addInput("%")}>x</button>
+        <button className='buttons' onClick={e => addInput("0")}>0</button>
+        <button className='buttons' onClick={e => addInput("+")}>+</button>
+        <button className='buttons' onClick={e => addInput("-")}>-</button>
+      </div>
     </div>
   );
 }
